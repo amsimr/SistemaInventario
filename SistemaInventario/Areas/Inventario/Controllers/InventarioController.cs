47a0377348118a93edd6a95461ab33454fdf7ae0
amsimr <amsi.medinadr@gmail.com>
2022-11-14 01:16:47 -0400

    Controller y Vista Stock producto

---
+++ SistemaInventario/Areas/Inventario/Controllers/InventarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.Utilidades;
+
+namespace SistemaInventario.Areas.Inventario.Controllers
+{
+    [Area("Inventario")]
+    [Authorize(Roles =DS.Role_Admin+","+DS.Role_Inventario)]
+    public class InventarioController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InventarioController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        #region API
+
+        [HttpGet]
+        public IActionResult ObtenerTodos()
+        {
+            var todos = _db.BodegaProducto.Include(b => b.Bodega).Include(p => p.Producto).ToList();
+            return Json(new {data = todos});
+        }
+
+        #endregion
+
+
+
+    }
+}
